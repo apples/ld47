6132a19d1391891086c47f12f1f0482ce764a989
Jeramy Harrison <dbralir@gmail.com>
2020-10-05 03:44:01 -0500

    Added explanations

--- data/scripts/gui/scene_gameplay/root.lua
+++ data/scripts/gui/scene_gameplay/root.lua
@@ -1,14 +1,30 @@
 local vdom = require('vdom')
+local linq = require('linq')
 
 return function(props)
-    local turn_name = 'None';
+    local turn_name = 'None'
+    local explanation = {}
 
-    if (props.turn == 0) then turn_name = 'SUMMON'
-    elseif (props.turn == 1) then turn_name = 'SET_ACTIONS'
+    if (props.turn == 0) then turn_name = 'SUMMON' explanation = {'Drag a movement card onto a fighter, or press Space to skip.'}
+    elseif (props.turn == 1) then turn_name = 'SET_ACTIONS' explanation = {'Toggle the action state of your fighters by clicking on them.', 'Press Space when done.'}
     elseif (props.turn == 2) then turn_name = 'AUTOPLAYER'
     elseif (props.turn == 3) then turn_name = 'ENEMY_SPAWN'
     elseif (props.turn == 4) then turn_name = 'ENEMY_MOVE'
     elseif (props.turn == 5) then turn_name = 'ATTACK'
+    end
+
+    for i,v in ipairs(explanation) do
+        explanation[i] = vdom.create_element(
+            'label',
+            {
+                halign='left',
+                valign='top',
+                top=80 + i * 24,
+                height = 24,
+                text = v,
+                color = '#fff'
+            }
+        )
     end
 
     return vdom.create_element('widget', { width = '100%', height = '100%' },
@@ -22,6 +38,7 @@ return function(props)
                 text = 'Turn: ' .. turn_name,
                 color = '#fff'
             }
-        )
+        ),
+        explanation
     )
 end
